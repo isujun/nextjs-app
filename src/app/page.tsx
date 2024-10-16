acb1ffc27fa86e60308380bf1a9d730e5a5956df
Ilham Rusydi Abd <abd.ilhamrusydi@gmail.com>
2024-10-16 14:20:44 +0700

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import { Footer } from "@/components/shared/footer";
+import { Form } from "@/components/shared/form";
+import { Header } from "@/components/shared/header";
+import { Profile } from "@/components/shared/profile";
 
-export default function Home() {
+
+export default function Page() {
   return (
-    <div className="text-white text-center min">
-      <p>This is next page</p>
+    <div>
+      <Header />
+      <Profile name="goi" age="12" address="vietam" gender="m" />
+      <Profile name="fui" age="13" address="malay" gender="m" />
+      <Profile name="xui" age="23" address="cini" gender="m" />
+      <Profile name="voio" age="14" address="vietam" gender="f" />
+      <Form />
+      <Footer />
     </div>
   )
 }
